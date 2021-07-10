ac9466fd8165ec1af4c2128c4ffe9686a7541b12
Goldlatz <85141403+Goldlatz@users.noreply.github.com>
2021-07-11 01:41:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const You = blackjackGame['you'];
 const Dealer = blackjackGame['dealer'];
 const hitSound = new Audio('/Blackjack-Game/Sound/hit.wav');
 const winSound = new Audio('/Blackjack-Game/Sound/win.mp3');
-const lossSound = new Audio(/'Blackjack-Game/Sound/aww.wav');
+const lossSound = new Audio('/Blackjack-Game/Sound/aww.wav');
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackhit);
 document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
